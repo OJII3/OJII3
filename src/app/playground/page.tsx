a60278550cca1a5321b903919e25e3acddfe36fc
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-07-21 10:25:18 +0900

    Delete src directory

--- src/app/playground/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Playground() {
-  return (
-    <div className="h-screen text-white bg-black">
-      <div className="flex flex-col items-center justify-center h-full animate-pulse">
-        Under Construction...
-      </div>
-    </div>
-  );
-}
--- src/lib/links.ts
+++
@@ -1,6 +0,0 @@
-const links = {
-  github: "https://github.com/ojii3",
-  twitter: "https://twitter.com/ojii3_dev",
-};
-
-export default links;
